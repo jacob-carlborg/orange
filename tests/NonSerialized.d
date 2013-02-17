2eb369006440bdfe7a0ec18832d677d2ff1c815d
Jacob Carlborg <doob@me.com>
2013-02-17 21:39:25 +0100

    Add test for non serialized fields using UDA's.

--- tests/NonSerialized.d
+++ tests/NonSerialized.d
@@ -19,7 +19,7 @@ XmlArchive!(char) archive;
 class Bar
 {
 	mixin NonSerialized;
-	
+
 	int c;
 }
 
@@ -27,8 +27,9 @@ class Foo
 {
 	int a;
 	int b;
+	@nonSerialized int c;
 	Bar bar;
-	
+
 	mixin NonSerialized!(a);
 }
 
@@ -38,10 +39,12 @@ unittest
 {
 	archive = new XmlArchive!(char);
 	serializer = new Serializer(archive);
-	
+
 	foo = new Foo;
 	foo.a = 3;
 	foo.b = 4;
+	foo.c = 5;
+
 	foo.bar = new Bar;
 
 	describe("serialize object with a non-serialized field") in {
@@ -51,16 +54,20 @@ unittest
 			assert(archive.data().containsDefaultXmlContent());
 			assert(archive.data().containsXmlTag("object", `runtimeType="tests.NonSerialized.Foo" type="tests.NonSerialized.Foo" key="0" id="0"`));
 			assert(archive.data().containsXmlTag("int", `key="b" id="1"`, "4"));
+
+			assert(!archive.data().containsXmlTag("int", `key="a" id="1"`, "3"));
+			assert(!archive.data().containsXmlTag("int", `key="c" id="3"`, "5"));
 			assert(!archive.data().containsXmlTag("object", `runtimeType="tests.NonSerialized.Bar" type="Bar" key="bar" id="2"`));
 		};
 	};
-	
+
 	describe("deserialize object with a non-serialized field") in {
 		it("short return deserialized object equal to the original object, where only one field is deserialized") in {
 			auto f = serializer.deserialize!(Foo)(archive.untypedData);
 
-			assert(foo.b == f.b);
 			assert(f.a == foo.a.init);
+			assert(f.b == foo.b);
+			assert(f.c == foo.c.init);
 			assert(f.bar is foo.bar.init);
 		};
 	};
