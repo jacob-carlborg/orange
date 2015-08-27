07c0df99bd8ca4ea4f61c6a91b0d7a370d5738de
Jacob Carlborg <doob@me.com>
2015-08-27 22:06:03 +0200

    Remove unused test utility functions

--- tests/Util.d
+++ tests/Util.d
@@ -24,55 +24,6 @@ bool contains (T) (T[] arr, T[] pattern)
     return !arr.find(pattern).empty;
 }
 
-bool containsDefaultXmlContent (string source)
-{
-    return source.containsXmlHeader() &&
-           source.containsArchive() &&
-           source.containsXmlTag("data");
-}
-
-bool containsXmlHeader (string source)
-{
-    return source.contains(`<?xml version="1.0" encoding="UTF-8"?>`);
-}
-
-bool containsArchive (string source)
-{
-    return source.containsArchiveHeader() && source.contains("</archive>");
-}
-
-bool containsArchiveHeader (string source)
-{
-    return source.contains(`<archive type="org.dsource.orange.xml" version="1.0.0">`) ||
-        source.contains(`<archive version="1.0.0" type="org.dsource.orange.xml">`);
-}
-
-bool containsXmlTag (string source, string tag, bool simple = false)
-{
-    return source.containsXmlTag(tag, null, null, simple);
-}
-
-bool containsXmlTag (string source, string tag, string attributes, bool simple = false)
-{
-    return source.containsXmlTag(tag, attributes, null, simple);
-}
-
-bool containsXmlTag (string source, string tag, string attributes, string content, bool simple = false)
-{
-    string pattern = '<' ~ tag;
-
-    if (attributes.length > 0)
-        pattern ~= ' ' ~ attributes;
-
-    if (simple)
-        return source.contains(pattern ~ "/>");
-
-    if (content.length > 0)
-        return source.contains(pattern ~ '>' ~ content ~ "</" ~ tag ~ '>');
-
-    return source.contains(pattern ~ '>') && source.contains("</" ~ tag ~ '>');
-}
-
 bool equalToXml(string expected, string actual)
 {
     import std.string;
