c1ad5b35d89d9d8365223729e743e855b4274b45
Jacob Carlborg <doob@me.com>
2015-08-27 21:26:48 +0200

    Use stable comparison for enum constant member tests

--- tests/EnumConstantMember.d
+++ tests/EnumConstantMember.d
@@ -32,12 +32,20 @@ unittest
 
     describe("serialize enum") in {
         it("shouldn't fail to compile when there is a constant enum member") in {
+            auto expected = q"xml
+<?xml version="1.0" encoding="UTF-8"?>
+<archive version="1.0.0" type="org.dsource.orange.xml">
+    <data>
+        <object runtimeType="tests.EnumConstantMember.G" type="tests.EnumConstantMember.G" key="0" id="0">
+            <int key="a" id="1">123</int>
+        </object>
+    </data>
+</archive>
+xml";
             serializer.reset();
             serializer.serialize(g);
 
-            assert(archive.data().containsDefaultXmlContent());
-            assert(archive.data().containsXmlTag("object", `runtimeType="tests.EnumConstantMember.G" type="tests.EnumConstantMember.G" key="0" id="0"`));
-            assert(archive.data().containsXmlTag("int", `key="a" id="1"`, "123"));
+            assert(expected.equalToXml(archive.data));
         };
     };
 
